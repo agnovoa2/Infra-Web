bec5ba4389f26bd58a9d88787ed813643784840f
Alejandro Gutiérrez Novoa <agnovoa2@esei.uvigo.es>
2016-05-23 13:34:52 +0200

    Added my printers frontend

--- src/main/java/es/uvigo/esei/infraestructura/controller/PrinterListController.java
+++ src/main/java/es/uvigo/esei/infraestructura/controller/PrinterListController.java
@@ -1,6 +1,8 @@
 package es.uvigo.esei.infraestructura.controller;
 
+import java.io.IOException;
 import java.security.Principal;
+import java.sql.SQLException;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +10,9 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 
+import es.uvigo.esei.infraestructura.entities.Model;
 import es.uvigo.esei.infraestructura.entities.Printer;
+import es.uvigo.esei.infraestructura.facade.ModelGatewayBean;
 import es.uvigo.esei.infraestructura.facade.PrinterGatewayBean;
 import es.uvigo.esei.infraestructura.facade.UserGatewayBean;
 
@@ -25,7 +29,14 @@ public class PrinterListController {
 	@Inject
 	private UserGatewayBean userGateway;
 	
+	@Inject
+	private ModelGatewayBean modelGateway;
+	
 	private int printer;
+	private String model;
+	private String ubication;
+	private boolean success = false;
+	private boolean error = false;
 	
 	@PostConstruct
     public void init() {
@@ -50,6 +61,42 @@ public class PrinterListController {
 	public void setPrinter(int printer) {
 		this.printer = printer;
 	}
+	
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public List<Model> getAllModels() {
+		return this.modelGateway.getAll();
+	}
+
+	public String getUbication() {
+		return ubication;
+	}
+
+	public void setUbication(String ubication) {
+		this.ubication = ubication;
+	}
+	
+	public boolean isError() {
+		return error;
+	}
+
+	public void setError(boolean error) {
+		this.error = error;
+	}
+	
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
 
 	public List<Printer> getNotProfessorPrinters() {
 		List<Printer> printers = this.printerGateway.getAll();
@@ -59,5 +106,17 @@ public class PrinterListController {
 	
 	public List<Printer> getAllProfessorPrinters(){
 		return this.userGateway.getCurrent().getPrinters();
+	}
+	
+	public void doAddPrinter() throws IOException {
+		try {
+			this.printerGateway.create(new Printer(getPrinter(), getUbication()));
+			this.printerGateway.getCurrent().setModel(modelGateway.find(getModel()));
+			this.printerGateway.save();
+
+			this.userGateway.getCurrent().getPrinters().add(this.printerGateway.getCurrent());
+			this.userGateway.save();
+		} catch (SQLException e) {
+		}
 	}
 }
